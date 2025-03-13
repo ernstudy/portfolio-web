a7bb3079b21033603e38c82a7a81c43737626973
ernstudy <hey@ernstudy.com>
2025-03-13 18:42:37 -0400

    fix bug in portfolio page

--- src/App.jsx
+++ src/App.jsx
@@ -11,12 +11,13 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 // comonents
 import AboutPage from "./pages/AboutPage";
 import EducationPage from "./pages/EducationPage";
-import PortfolioPage from "./pages/PortfolioPage";
+
 import ContactPage from "./pages/ContactPage";
 import SideBar from "./components/SideBar";
 import MobileHeader from "./components/MobileHeader";
 import ProjectDisplay from "./pages/ProjectDisplay/ProjectDisplay";
 import Footer from "./components/header_footer/Footer";
+import PortfolioPage from "./pages/PortfolioPage";
 
 export default function App() {
   return (
@@ -30,8 +31,6 @@ export default function App() {
             <Route path="/education" element={<EducationPage />} />
             <Route path="/portfolio" element={<PortfolioPage />} />
             <Route path="/portfolio/:id" element={<ProjectDisplay />} />
-            <Route path="/portfolio/project" element={<PortfolioPage />} />
-            <Route path="/portfolio/projects" element={<PortfolioPage />} />
             <Route path="/contact" element={<ContactPage />} />
           </Routes>
         </div>
